eb3269071ba0bb17d31ba5cb01e3b6a74bd5df4a
TijsGroenendaal <tijs.groenendaal@gmail.com>
2022-02-22 15:28:58 +0100

    🔧 create color palette

---
+++ lib/common/QueueMusicColor.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class QueueMusicColor {
+  static const black500 = Color(0xFF282828);
+  static const black600 = Color(0xFF202020);
+  static const black750 = Color(0xFF121212);
+  static const black    = Color(0xFF191414);
+  static const grey     = Color(0xFFB3B3B3);
+  static const white    = Color(0xFFFFFFFF);
+  static const red      = Color(0xFFF44337);
+  static const green    = Color(0xFF1D8954);
+  static const green750 = Color(0xFF1DA053);
+  static const green500 = Color(0xFF1DC853);
+}
