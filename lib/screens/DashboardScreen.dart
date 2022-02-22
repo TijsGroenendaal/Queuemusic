b3866b8503e79634465ac8ced4e764c8e63a99c3
TijsGroenendaal <tijs.groenendaal@gmail.com>
2022-02-22 15:29:33 +0100

    💩 setup main screen

---
+++ lib/screens/DashboardScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class DashboardScreen extends StatefulWidget {
+  static const route = "Dashboard";
+
+  const DashboardScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DashboardScreen> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<DashboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
