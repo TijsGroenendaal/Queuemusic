a169991320afff55a7a97b3c9598638f88e20ab9
TijsGroenendaal <tijs.groenendaal@gmail.com>
2022-03-01 15:36:11 +0100

    🎨 update color naming

--- lib/common/QueueMusicColor.dart
+++ lib/common/QueueMusicColor.dart
@@ -7,8 +7,8 @@ class QueueMusicColor {
   static const black    = Color(0xFF191414);
   static const grey     = Color(0xFFB3B3B3);
   static const white    = Color(0xFFFFFFFF);
-  static const red      = Color(0xFFF44337);
+  static const error    = Color(0xFFF44337);
   static const green    = Color(0xFF1D8954);
   static const green750 = Color(0xFF1DA053);
-  static const green500 = Color(0xFF1DC853);
+  static const complete = Color(0xFF1DC853);
 }
