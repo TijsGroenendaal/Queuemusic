bb45209d1fb702528563631b3cd9344741fb50ac
TijsGroenendaal <tijs.groenendaal@gmail.com>
2022-03-28 16:25:31 +0200

    🧪 add simple test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,20 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 
-import 'package:queuemusic/main.dart';
+import 'package:queuemusic/main.dart' as app;
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  group('test-group-1', () {
+    testWidgets('test one', (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      expect(find.text('Liked Songs'), findsOneWidget);
+    });
   });
 }
