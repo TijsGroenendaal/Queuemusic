844fd2dbdbc034357ec15ad3d1e17fac7ea07efd
TijsGroenendaal <tijs.groenendaal@gmail.com>
2022-04-05 13:40:15 +0200

    🚧 create simple dashboard screen

--- lib/screens/DashboardScreen.dart
+++ lib/screens/DashboardScreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:queuemusic/common/QueueMusicColor.dart';
+import 'package:queuemusic/common/QueueMusicTheme.dart';
 import '../widgets/LikedSongsWidget.dart';
 
 class DashboardScreen extends StatefulWidget {
@@ -11,8 +14,45 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardState extends State<DashboardScreen> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Center(child: Text("DASHBOARD")),
+    LikedSongsWidget(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const LikedSongsWidget();
+    return Theme(
+        data: theme(),
+        child: Scaffold(
+          body: _widgetOptions[_selectedIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _selectedIndex,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.house_fill),
+                label: "Dashboard"
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.heart_fill),
+                label: 'LikedSongs'
+              )
+            ],
+            selectedItemColor: QueueMusicColor.green,
+            selectedIconTheme: IconThemeData(color: QueueMusicColor.green750),
+            unselectedItemColor: QueueMusicColor.grey,
+            unselectedIconTheme: IconThemeData(color: QueueMusicColor.grey),
+            selectedFontSize: 12,
+            backgroundColor: QueueMusicColor.black750,
+            onTap: _onItemTapped,
+          ),
+        )
+    );
   }
 }
