4831aacb917a0a44857068d809d6c381d36219fc
TijsGroenendaal <tijs.groenendaal@gmail.com>
2022-04-15 15:54:00 +0200

    ⚡️ optimize imports

--- lib/models/Session.dart
+++ lib/models/Session.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
 import 'package:queuemusic/helper/IdHelper.dart';
-import 'package:uuid/uuid.dart';
 
 class Session with ChangeNotifier implements Exception {
   String error() => "Not In Session";
